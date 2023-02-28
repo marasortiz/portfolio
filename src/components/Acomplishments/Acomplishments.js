3a70937afd9831047b720ba34c75cf644535fd00
Mara <santamaria.ortiz.tamara@gmail.com>
2023-02-28 18:08:27 +0100

    Acomplishments Ended

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,19 +1,34 @@
 import React from 'react';
 
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  // Examples
+  // { number: 20, text: 'Open Source Projects'},
+  // { number: 1000, text: 'Students', },
+  // { number: 1900, text: 'Github Followers', },
+  { number: 20, text: 'Github Proyects', }
 ];
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+  <Section>
+    <SectionTitle>
+      Personal Acomplishments
+    </SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>
+            {card.number}+
+          </BoxNum>
+          <BoxText>
+            {card.text}
+          </BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
 );
 
 export default Acomplishments;
