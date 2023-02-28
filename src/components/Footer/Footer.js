a59994e7ca18a4592791943bd29527322b0eab2e
Mara <santamaria.ortiz.tamara@gmail.com>
2023-02-28 18:18:54 +0100

    Footer Ended

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      {/* Data */}
+      <LinkList>
+        {/* Phone number */}
+        {/* <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:111-111-1111">111-111-1111</LinkItem>
+        </LinkColumn> */}
+        <LinkColumn>
+          {/* Mail */}
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:santamaria.ortiz.tamara@gmail.com">
+            santamaria.ortiz.tamara@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        {/* Social Icons Links */}
+        <SocialContainer>
+          <SocialIcons href="https://github.com/marasortiz">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          {/* <SocialIcons href="https://linkedin.com">
+                <AiFillLinkedin size="3rem" />
+              </SocialIcons> */}
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
