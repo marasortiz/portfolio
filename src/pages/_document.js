1c835b6741e7a6db1ed2ac0e1f4db56ba45a24ec
Mara <santamaria.ortiz.tamara@gmail.com>
2023-03-01 13:27:05 +0100

    Add 'title' name

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -29,9 +29,13 @@ export default class MyDocument extends Document {
   }
   render() {
     return (
-      <Html lang='en-GB'>
+      <Html lang="en-GB">
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+          <title>Portfolio</title>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
