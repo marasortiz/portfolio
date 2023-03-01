948a613bb73f20d3266fae3d571bb0b38fc6b63d
Mara <santamaria.ortiz.tamara@gmail.com>
2023-03-01 10:33:03 +0100

    Fix bug for IMG

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -30,7 +30,7 @@ const Projects = () => (
       {projects.map(
         ({ id, image, title, description, tags, source, visit }) => (
           <BlogCard key={id}>
-            <Img src={image} />
+            <Img src={`portfolio/${image}`} />
             <TitleContent>
               <HeaderThree title> {title} </HeaderThree>
               <Hr />
